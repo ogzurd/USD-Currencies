4af3a7b1b0cc1d18326c533d510390f442b9a209
ogzurd <57904809+ogzurd@users.noreply.github.com>
2022-02-16 21:21:15 +0300

    commited

---
+++ main.py
@@ -0,0 +1,27 @@
+import datetime
+
+import pandas as pd
+from forex_python.converter import CurrencyRates
+import pandas as pd
+c = CurrencyRates()
+
+date = pd.date_range('2021-01-01', '2021-01-10', freq='D')
+
+dates = []
+names = []
+values = []
+
+for d in date:
+    for name, value in c.get_rates('USD', d.date()).items():
+        dates.append(d.date())
+        names.append(name)
+        values.append(value)
+
+df = pd.DataFrame({"Date":dates, "Names":names, "Values": values})
+df.to_csv(r"C:\Users\ozgur\Desktop\USD-Currency\Currencies.csv", index=False)
+
+
+
+
+
+
